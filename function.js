2996b4c5f917e07d5aeadb72c76b5f8e03d3a515
hunor64 <nhunor06@gmail.com>
2024-09-12 09:47:51 +0200

    all element

--- function.js
+++ function.js
@@ -1,6 +1,11 @@
 function getFirstElement(arr) {
   return arr[0];
 }
+function writeAllElements(arr) {
+  arr.forEach((element) => {
+    console.log(element);
+  });
+}
 
 const numbers = [1, 2, 3];
 const words = ["apple", "banana", "cherry"];
